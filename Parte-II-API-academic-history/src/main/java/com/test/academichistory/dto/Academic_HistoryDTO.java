5d37e351cbd599a96916511020c71d956211af08
Alejandro Dueñas <luisdue3@gmail.com>
2024-04-25 09:05:13 -0500

    Se agregan comentarios a Academic_HistoryDTO

--- Parte-II-API-academic-history/src/main/java/com/test/academichistory/dto/Academic_HistoryDTO.java
+++ Parte-II-API-academic-history/src/main/java/com/test/academichistory/dto/Academic_HistoryDTO.java
@@ -5,6 +5,7 @@ import com.test.academichistory.model.Academic_History;
 import java.math.BigDecimal;
 
 public class Academic_HistoryDTO {
+  // ATRIBUTOS
   private Long progress_id;
   private Integer year;
   private Integer period;
@@ -12,6 +13,7 @@ public class Academic_HistoryDTO {
   private Long student_id;
   private Long course_id;
 
+  // CONSTRUCTOR
   public Academic_HistoryDTO(Long progress_id, Integer year, Integer period, BigDecimal note, Long student_id, Long course_id) {
     this.progress_id = progress_id;
     this.year = year;
@@ -21,7 +23,7 @@ public class Academic_HistoryDTO {
     this.course_id = course_id;
   }
 
-  // Construir un objeto academic_history
+  // Construir un objeto academic_history con los mismos datos que recibe de academic_historyDTO
   public Academic_History setAcademic_History(Academic_HistoryDTO academic_historyDTO){
     Academic_History academic_History = new Academic_History();
     academic_History.setId(academic_historyDTO.getProgress_id());
