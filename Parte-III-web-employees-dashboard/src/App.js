f163565cf7b97925124b65bbab685d75fda6e24a
Alejandro Dueñas <luisdue3@gmail.com>
2024-04-24 20:00:56 -0500

    Parte III de la prueba

---
+++ Parte-III-web-employees-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeesDashboard from './pages/EmployeesDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeesDashboard/>
+    </div>
+  );
+}
+
+export default App;
