0ff2b8a6f5207d07e98579dfc5c2a4f5f16700af
Alejandro Dueñas <luisdue3@gmail.com>
2024-04-24 21:10:30 -0500

    Se cambia a identación a 2 espacios

--- Parte-III-web-employees-dashboard/src/components/EmployeesTable.jsx
+++ Parte-III-web-employees-dashboard/src/components/EmployeesTable.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 
 export default function EmployeesTable({ employees, filterField, searchTerm }) {
-    const filteredEmployees = employees.filter((employee) => {
-        if (filterField === 'name') {
-            return employee.name.toLowerCase().includes(searchTerm.toLowerCase());
-        } else if (filterField === 'email') {
-            return employee.email.toLowerCase().includes(searchTerm.toLowerCase());
-        } else if (filterField === 'city') {
-            return employee.address.city.toLowerCase().includes(searchTerm.toLowerCase());
-        }
-        return true; // Mostrar todos si no hay filtro aplicado
-    });
+  const filteredEmployees = employees.filter((employee) => {
+    if (filterField === 'name') {
+      return employee.name.toLowerCase().includes(searchTerm.toLowerCase());
+    } else if (filterField === 'email') {
+      return employee.email.toLowerCase().includes(searchTerm.toLowerCase());
+    } else if (filterField === 'city') {
+      return employee.address.city.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+    return true; // Mostrar todos si no hay filtro aplicado
+  });
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Company</th>
-                        <th>City</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredEmployees.map((employee) => (
-                        <tr key={employee.id}>
-                            <td>{employee.name}</td>
-                            <td>{employee.username}</td>
-                            <td>{employee.email}</td>
-                            <td>{employee.company.name}</td>
-                            <td>{employee.address.city}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredEmployees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.name}</td>
+              <td>{employee.username}</td>
+              <td>{employee.email}</td>
+              <td>{employee.company.name}</td>
+              <td>{employee.address.city}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- Parte-III-web-employees-dashboard/src/pages/EmployeesDashboard.jsx
+++ Parte-III-web-employees-dashboard/src/pages/EmployeesDashboard.jsx
@@ -2,51 +2,51 @@ import React, { useEffect, useState } from 'react'
 import EmployeesTable from '../components/EmployeesTable'
 
 export default function EmployeesDashboard() {
-    const [employees, setEmployees] = useState([])
-    const [filterField, setFilterField] = useState('name'); // Campo inicial para filtrar (por nombre)
-    const [searchTerm, setSearchTerm] = useState(''); // Término de búsqueda
-    useEffect(()=>{
-        const fetchEmployees = async ()=>{
-            try {
-                const response = await fetch("https://jsonplaceholder.typicode.com/users")
-                const data = await response.json()
-                console.log(data);
-                setEmployees(data)
-            } catch (error) {
-                console.log("Error calling employees API", error)
-            }
-        }
-        fetchEmployees()
-    }, [])
-    const handleFilterChange = (e) => {
-      setFilterField(e.target.value);
+  const [employees, setEmployees] = useState([])
+  const [filterField, setFilterField] = useState('name'); // Campo inicial para filtrar (por nombre)
+  const [searchTerm, setSearchTerm] = useState(''); // Término de búsqueda
+  useEffect(()=>{
+    const fetchEmployees = async ()=>{
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+        console.log(data);
+        setEmployees(data)
+      } catch (error) {
+        console.log("Error calling employees API", error)
+      }
+    }
+    fetchEmployees()
+  }, [])
+  const handleFilterChange = (e) => {
+    setFilterField(e.target.value);
   };
 
   const handleSearchChange = (e) => {
-      setSearchTerm(e.target.value);
+    setSearchTerm(e.target.value);
   };
   return (
     <div>
       <h1>Employees List</h1>
       <div>
-                <label htmlFor="filterField">Filter by:</label>
-                <select id="filterField" value={filterField} onChange={handleFilterChange}>
-                    <option value="name">Name</option>
-                    <option value="email">Email</option>
-                    <option value="city">City</option>
-                </select>
-                <input
-                    type="text"
-                    placeholder={`Search by ${filterField}`}
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                />
-            </div>
-            {employees.length > 0 ? (
-                <EmployeesTable employees={employees} filterField={filterField} searchTerm={searchTerm} />
-            ) : (
-                <p>Loading...</p>
-            )}
+        <label htmlFor="filterField">Filter by:</label>
+        <select id="filterField" value={filterField} onChange={handleFilterChange}>
+          <option value="name">Name</option>
+          <option value="email">Email</option>
+          <option value="city">City</option>
+        </select>
+        <input
+          type="text"
+          placeholder={`Search by ${filterField}`}
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+      {employees.length > 0 ? (
+        <EmployeesTable employees={employees} filterField={filterField} searchTerm={searchTerm} />
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
